68daffd8b5793ebff5c331bd541f7f89074277f9
Khang Hoang Tran <tranhoangkhang09112001@gmail.com>
2025-12-20 11:59:46 +0700

    feat-velero-bk-rs: Add backup & restore steps

--- script/2.velero--backup-and-restore/backup.sh
+++ script/2.velero--backup-and-restore/backup.sh
@@ -4,7 +4,7 @@ function main(){
     source <(curl -sS https://raw.githubusercontent.com/khangtictoc/Productive-Workspace-Set-Up/refs/heads/main/linux/utility/library/bash/ansi_color.sh)
     init-ansicolor
 
-    current_time=$(date +"%Y/%m/%d-%H:%M:%S")
+    current_time=$(date +"%Y.%m.%d-%H.%M.%S")
     cluster_name="$1"
     backup_file="$cluster_name-$current_time"
 
