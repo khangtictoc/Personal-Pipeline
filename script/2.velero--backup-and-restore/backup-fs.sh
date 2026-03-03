0435cf68002b356692d185345aa833378be192ed
Khang Hoang Tran <tranhoangkhang09112001@gmail.com>
2026-03-04 01:38:25 +0700

    feat: Add options for backup/restore with restic/kopia or not

---
+++ script/2.velero--backup-and-restore/backup-fs.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+function main(){
+    source <(curl -sS https://raw.githubusercontent.com/khangtictoc/Productive-Workspace-Set-Up/refs/heads/main/linux/utility/library/bash/ansi_color.sh)
+    init-ansicolor
+
+    current_time=$(date +"%Y.%m.%d-%H.%M.%S")
+    cluster_name="$1"
+    cluster_name_formatted=$(echo "$cluster_name" | tr '[:upper:]' '[:lower:]')
+    backup_file="$cluster_name_formatted-$current_time"
+
+    echo "[INFO] Start fully backup - ${YELLOW}Cluster: $cluster_name, Recorded at: $current_time ${NC} ..."
+    velero create backup  --include-cluster-scoped-resources="*" --include-namespaces="*" $backup_file
+    echo "${GREEN}[SUCCESS] Backup Completed!${NC}"
+
+    echo "[INFO] Confirm your backups:"
+    velero backup get | grep --color=always "$backup_file"
+
+    echo "[INFO] Monitoring restoring logs"
+    velero backup logs $backup_file 
+}
+
+main "$@"
---
+++ script/2.velero--backup-and-restore/restore-fs.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+function main(){
+    source <(curl -sS https://raw.githubusercontent.com/khangtictoc/Productive-Workspace-Set-Up/refs/heads/main/linux/utility/library/bash/ansi_color.sh)
+    init-ansicolor
+
+    echo "[INFO] Start fully restore ..."
+    restore_version="$1"
+    velero restore create $restore_version --from-backup $restore_version --wait
+    echo "${GREEN}[SUCCESS] Restore Completed!${NC}"
+
+    echo "[INFO] Confirm your restorations:"
+    velero restore get | grep --color=always "$restore_version"
+
+    echo "[INFO] Monitoring restoring logs"
+    velero restore logs $restore_version
+}
+
+main "$@"
